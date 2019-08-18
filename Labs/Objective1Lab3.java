3159028e284b2ac7e59985500f9e9a0aadd4f62a
Noel Castillo <Castillo752x@gmail.com>
2019-08-17 23:20:23 -0400

    Completed Objective 1 Lab 3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,7 @@
+public class Objective1Lab3{
+  public static void main( String[] args ){
+  System.out.println( "Name : Noel Castillo");
+  System.out.println( "Age  : 25" );
+  System.out.println( "State  : Massachusetts" );
+  }
+}
