5a3416289ca12907ff55e697b235cc39affa9cde
Noel Castillo <Castillo752x@gmail.com>
2019-08-18 00:09:33 -0400

    Completed Objective2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Noel Castillo";
+    int age = 25;
+    String state = "Massachusetts";
+
+    System.out.print( "Name: " + name + " Age: " + age + "  State: " + state );
+    
+  }
+}
